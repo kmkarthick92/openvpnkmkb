02329b2be7da0d5bba50d23e91623f458f42fc8f
kmkarthick92 <kmkarthick92@gmail.com>
2024-08-10 19:24:00 +0530

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -2,6 +2,10 @@
 
 # Add Docker's official GPG key:
 sudo apt-get update
+sudo apt-get install cockpit -y
+sudo apt-get install cockpit-pcp -y
+sudo systemctl enable --now cockpit.socket
+sudo systemctl start cockpit
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
